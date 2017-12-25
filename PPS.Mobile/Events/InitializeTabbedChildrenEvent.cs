9e7ac601f0dd30e5b5abf2e1552bf6710d4e5d09
Joel Dean <jdeanjj1000@gmail.com>
2017-12-25 14:04:31 -0500

    added tabbed navigation base constructs

---
+++ PPS.Mobile/Events/InitializeTabbedChildrenEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Prism.Events;
+using Prism.Navigation;
+
+namespace PPS.Mobile.Events
+{
+    public class InitializeTabbedChildrenEvent : PubSubEvent<NavigationParameters>
+    {
+
+    }
+}
---
+++ PPS.Mobile/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Navigation;
+
+namespace PPS.Mobile.ViewModels
+{
+    public class BaseViewModel : BindableBase, INavigatingAware, IDestructible
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public virtual void Destroy()
+        {
+        }
+
+        public virtual void OnNavigatingTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
