efb5143dd1885326cacdee3851fdeff544c35134
Joel Dean <jdeanjj1000@gmail.com>
2018-03-14 17:12:49 -0500

    Started the Issue View Model that will store all the data and properties that are to be binded to the Telerik DataForm

--- PPS.Mobile/Data/Models/Issue.cs
+++ PPS.Mobile/Data/Models/Issue.cs
@@ -6,8 +6,13 @@ namespace PPS.Mobile.Data.Models
         public string IssueId { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+        public string Location { get; set; }
         public DateTime SubmittedOn { get; set; }
         public string IssueType { get; set; }
+        public string Comment { get; set; }
+        public string SubmittedBy { get; set; }
+        public string SubmitterId { get; set; }
+        public string SUbmitterIdType { get; set; }
         
     }
 }
---
+++ PPS.Mobile/ViewModels/IssueFormViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Telerik.XamarinForms.Common;
+using Telerik.XamarinForms.Common.DataAnnotations;
+
+namespace PPS.Mobile.ViewModels
+{
+    public class IssueFormViewModel:NotifyPropertyChangedBase
+    {
+        int severity;
+        string comment;
+        string location;
+        [ReadOnly]
+        DateTime submittedOn = DateTime.UtcNow;
+
+        [DisplayOptions(Position = 0, ColumnSpan = 2, PlaceholderText = "Location", Group = "Issue")]
+        [NonEmptyValidatorAttribute("Location is required!")]
+        public string Location
+        {
+            get
+            {
+                return this.location;
+            }
+            set
+            {
+                if (this.location != value)
+                {
+                    this.location = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        [DisplayOptions(Header = "Submitted At", Position = 2, ColumnPosition = 1, PlaceholderText = "Time", Group = "Issue")]
+        [DisplayValueFormat(Time = "hh:mm tt")]
+        [NativeConversionContext(DateTimeKind.Local)]
+        public DateTime SubmittedAt
+        {
+            get
+            {
+                return this.submittedOn;
+            }
+            set
+            {
+                if (this.submittedOn != value)
+                {
+                    this.submittedOn = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        [DisplayOptions(Header = "Submitted On", Position = 1, ColumnPosition = 1, PlaceholderText = "Date", Group = "Issue")]
+        [DisplayValueFormat(Date = "MMMM dd, yyyy")]
+        [NativeConversionContext(DateTimeKind.Local)]
+        public DateTime SubmittedOn
+        {
+            get
+            {
+                return this.submittedOn;
+            }
+            set
+            {
+                if (this.submittedOn != value)
+                {
+                    this.submittedOn = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
